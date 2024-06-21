77061c3c745c339e2441639ecdb7102f763c8458
Richard T. Jones <jonesrt@gluey.phys.uconn.edu>
2024-06-21 10:25:36 -0400

    * learning how to write a correct bat script [rtj]

--- scripts/openssl_win32_installer.bat
+++ scripts/openssl_win32_installer.bat
@@ -10,11 +10,11 @@ REM version: june 5, 2024
 
 setlocal
 
-set openssl_version=3.2.1
-set openssl_tarball_url="https://www.openssl.org/source/openssl-%openssl_version%.tar.gz"
-set nasm_version=2.16
-set nasm_zip_url="https://www.nasm.us/pub/nasm/releasebuilds/%NASM_VERSION%/win64/nasm-%NASM_VERSION%-win64.zip"
-set nasm_zip_url="https://www.nasm.us/pub/nasm/releasebuilds/%NASM_VERSION%/win32/nasm-%NASM_VERSION%-win32.zip"
+set "openssl_version=3.2.1"
+set "openssl_tarball_url=https://www.openssl.org/source/openssl-%openssl_version%.tar.gz"
+set "nasm_version=2.16"
+set "nasm_zip_url=https://www.nasm.us/pub/nasm/releasebuilds/%nasm_version%/win64/nasm-%nasm_version%-win64.zip"
+set "nasm_zip_url=https://www.nasm.us/pub/nasm/releasebuilds/%nasm_version%/win32/nasm-%nasm_version%-win32.zip"
 
 for /f "delims=" %%i in ('powershell -Command "$input = '%2'; $pattern = '\\Community\\.*'; $replacement = '\Community\VC\Auxiliary\Build\vcvarsall.bat'; $result = [regex]::Replace($input, $pattern, $replacement); Write-Output $result"') do set "vcvarsall_bat1=%%i"
 for /f "delims=" %%i in ('powershell -Command "$input = '%vcvarsall_bat1%'; $pattern = '\\Enterprise\\.*'; $replacement = '\Enterprise\VC\Auxiliary\Build\vcvarsall.bat'; $result = [regex]::Replace($input, $pattern, $replacement); Write-Output $result"') do set "vcvarsall_bat=%%i"
@@ -27,23 +27,17 @@ curl -L -o nasm.zip "%nasm_zip_url%"
 echo "install into %1\nasm-%nasm_version%"
 powershell -Command "& {Expand-Archive -Path nasm.zip -DestinationPath %1 -Force}"
 del nasm.zip
-set PATH="%1\nasm-%sasm_version%;%PATH%"
+set "PATH=%1\nasm-%nasm_version%;%PATH%"
 echo "PATH is %PATH%"
-echo "dir %1 is"
-dir "%1"
-echo "dir %1\nasm-%nasm_version% is"
-dir "%1\nasm-%nasm_version%"
-echo Verifying NASM installation...
 nasm -v
-nasm.exe -v
 "%1\nasm-%nasm_version%\nasm" -v
 echo NASM installation completed.
 
 REM curl -L %openssl_tarball_url% -o "openssl-%openssl_version%.tar.gz"
 REM tar -zxf openssl-%openssl_version%.tar.gz
-set source="openssl-%openssl_version%"
+set "source=openssl-%openssl_version%"
 cd %source%
-set PATH="%1\perl\bin;%PATH%"
+set "PATH=%1\perl\bin;%PATH%"
 perl --version
 perl Configure no-shared --prefix="%1" --openssldir="%1"
 nmake VERBOSE=1 INST_TOP="%1"
