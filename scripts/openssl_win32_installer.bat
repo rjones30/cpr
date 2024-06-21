65a4263a6336565979022d2bd94588040ed18aac
Richard T. Jones <jonesrt@gluey.phys.uconn.edu>
2024-06-21 09:57:56 -0400

    * to invoke a bat script from within a bat script, use "call" [rtj]

--- scripts/openssl_win32_installer.bat
+++ scripts/openssl_win32_installer.bat
@@ -16,13 +16,9 @@ set nasm_version=2.16
 set nasm_zip_url="https://www.nasm.us/pub/nasm/releasebuilds/%NASM_VERSION%/win64/nasm-%NASM_VERSION%-win64.zip"
 set nasm_zip_url="https://www.nasm.us/pub/nasm/releasebuilds/%NASM_VERSION%/win32/nasm-%NASM_VERSION%-win32.zip"
 
-echo "Incoming path to cl is %2"
-for /f "delims=" %%i in ('powershell -Command "$input = '%2'; $pattern = '\\Community\\.*'; $replacement = '\\Community\\VC\\Auxiliary\\Build\\vcvarsall.bat'; $result = [regex]::Replace($input, $pattern, $replacement); Write-Output $result"') do set "vcvarsall_bat1=%%i"
-echo "After first revision, path to vcvarsall_bat is %vcvarsall_bat1%"
-for /f "delims=" %%i in ('powershell -Command "$input = '%vcvarsall_bat1%'; $pattern = '\\Enterprise\\.*'; $replacement = '\\Enterprise\\VC\\Auxiliary\\Build\\vcvarsall.bat'; $result = [regex]::Replace($input, $pattern, $replacement); Write-Output $result"') do set "vcvarsall_bat=%%i"
-echo "After final revision, path to vcvarsall_bat is %vcvarsall_bat%"
-REM "c:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
-"%vcvarsall_bat%" x86_amd64
+for /f "delims=" %%i in ('powershell -Command "$input = '%2'; $pattern = '\\Community\\.*'; $replacement = '\Community\VC\Auxiliary\Build\vcvarsall.bat'; $result = [regex]::Replace($input, $pattern, $replacement); Write-Output $result"') do set "vcvarsall_bat1=%%i"
+for /f "delims=" %%i in ('powershell -Command "$input = '%vcvarsall_bat1%'; $pattern = '\\Enterprise\\.*'; $replacement = '\Enterprise\VC\Auxiliary\Build\vcvarsall.bat'; $result = [regex]::Replace($input, $pattern, $replacement); Write-Output $result"') do set "vcvarsall_bat=%%i"
+call "%vcvarsall_bat%" x86_amd64
 
 nmake -P
 
